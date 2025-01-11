4460b6a778641116109e47ca90c3ba3a3adb6213
shounak <shounakbose2000@gmail.com>
2025-01-11 12:05:08 +0530

    Learn About Dataypes

---
+++ 01_Basic/02_Datatype.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) we are use node here 
+
+console.log(3+3);
+console.log("Shounak");
+
+let name = "Shounak";
+let age = 24;
+let isLogin = true;
+
+console.log(typeof name);
+
+console.log(typeof age);
+
+console.log(typeof isLogin);
+
+console.log(typeof null); 
